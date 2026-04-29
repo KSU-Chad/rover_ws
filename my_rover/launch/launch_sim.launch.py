6bdd69effbb639c0485d753a142cc26b61622852
KSU-Chad <37449415+KSU-Chad@users.noreply.github.com>
2026-04-29 00:18:04 -0500

    Update launch_sim.launch.py

--- my_rover/launch/launch_sim.launch.py
+++ my_rover/launch/launch_sim.launch.py
@@ -66,7 +66,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true', 'my_controller_yaml_path': controllers_yaml}.items()
              )
 
 
